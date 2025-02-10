0655293765e1ed6b1e80fd7d347b763238f2e0a2
lautarogregorat <lautarogregorat@gmail.com>
2025-02-10 19:52:39 -0300

    infra add

---
+++ orderservice/src/main/java/wtas/api/microservices/orderservice/config/RestClientConfig.java
@@ -0,0 +1,45 @@
+package wtas.api.microservices.orderservice.config;
+
+import io.micrometer.observation.ObservationRegistry;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import wtas.api.microservices.orderservice.client.InventoryClient;
+
+import java.time.Duration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RestClientConfig {
+
+    @Value("${inventory.url}")
+    private String inventoryServiceUrl;
+
+    private final ObservationRegistry observationRegistry;
+
+    @Bean
+    public InventoryClient inventoryClient() {
+        RestClient restClient = RestClient.builder()
+                .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
+                .observationRegistry(observationRegistry)
+                .build();
+        var restClientAdapter = RestClientAdapter.create(restClient);
+        var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout((int) Duration.ofSeconds(3).toMillis());
+        factory.setReadTimeout((int) Duration.ofSeconds(3).toMillis());
+        return factory;
+    }
+
+}
